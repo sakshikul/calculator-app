79eafd78e702989e2248e5779a9870df236ab16f
Sakshi Kulshrestha <kulsakshi24@gmail.com>
2019-05-07 21:38:55 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from "./Calculator";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Calculator App</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Calculator.css";
+
+
+class Calculator extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            calculator : "clear",
+            result : 0
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+
+    componentWillUnmount(){
+
+    }
+    render(){
+        return(
+            <div className = "calculator">
+            <table className = "calculator__table">
+                <tbody className = "calculator__table__body">
+                <tr  className = "result row">
+                    <th colspan="4">{this.state.result}</th>
+                </tr>
+                <tr className = "clear row">
+                    <td colspan="3">{this.state.calculator}</td>
+                    <td>&divide; </td>
+                </tr>
+                <tr className = "row">
+                   <td>7</td>
+                   <td>8</td>
+                   <td>9</td>  
+                   <td>-</td>
+                </tr>
+                <tr className = "row">
+                   <td>4</td>
+                   <td>5</td>
+                   <td>6</td>
+                   <td> + </td>
+                </tr>
+                <tr className = "row">
+                   <td>1</td>
+                   <td>2</td>
+                   <td>3</td>
+                   <td> = </td>
+                </tr>
+                </tbody>
+            </table>
+            
+            </div>
+        )
+    }
+
+
+
+}
+
+
+export default Calculator;
