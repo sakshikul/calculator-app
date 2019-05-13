bbedd3db0e5ab2bdb14d988eceda5696fa3023d3
Sakshi Kulshrestha <kulsakshi24@gmail.com>
2019-05-14 03:02:33 +0530

    print the value

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,59 +5,45 @@ import "./Calculator.css";
 class Calculator extends React.Component{
     constructor(props){
         super(props)
-        this.state= {
-            calculator : "clear",
-            result : 0
-        }
-        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+           result : ""
     }
-
-    componentDidMount(){
-
+       
+    this.handleClick = this.handleClick.bind(this)
     }
+    
+    
+    handleClick(x) {
+     var s = x.target.name;
+     this.setState({
+         result :  this.state.result + s
+         
+     })
+   }
 
-
-    componentWillUnmount(){
-
-    }
-    handleClick(){
-        console.log("typed number");
-        
-    }
-    render(){
+  
+   
+    render(){  
+       
         return(
+
             <div className = "calculator">
-            <table className = "calculator__table">
-                <tbody className = "calculator__table__body">
-                <tr  className = "result row">
-                    <th colSpan="4">{this.state.result}</th>
-                </tr>
-                <tr className = "clear row">
-                    <td colSpan="2">{this.state.calculator}</td>
-                    <td onClick = {this.handleClick}>0</td>
-                    <td onClick = {this.handleClick} className= "red">&divide; </td>
-                </tr>
-                <tr className = "row">
-                   <td onClick = {this.handleClick}>7</td>
-                   <td onClick = {this.handleClick}>8</td>
-                   <td onClick = {this.handleClick}>9</td>  
-                   <td onClick = {this.handleClick} className = "red">-</td>
-                </tr>
-                <tr className = "row">
-                   <td onClick = {this.handleClick}>4</td>
-                   <td onClick = {this.handleClick}>5</td>
-                   <td onClick = {this.handleClick}>6</td>
-                   <td onClick = {this.handleClick} className = "red"> + </td>
-                </tr>
-                <tr className = "row">
-                   <td onClick = {this.handleClick}>1</td>
-                   <td onClick = {this.handleClick}>2</td>
-                   <td onClick = {this.handleClick}>3</td>
-                   <td onClick = {this.handleClick} className = "red"> = </td>
-                </tr>
-                </tbody>
-            </table>
-            
+                <button name = "result" className ="result" onClick = {this.handleClick}>{this.state.result}</button>
+                <button name = "1" onClick = {this.handleClick}>1</button>
+                <button name ="2" onClick = {this.handleClick}>2</button>
+                <button name ="3" onClick = {this.handleClick}>3</button>
+                <button name = "4" onClick = {this.handleClick}>4</button>
+                <button name ="5" onClick = {this.handleClick}>5</button>
+                <button name ="6" onClick = {this.handleClick}>6</button>
+                <button name ="7" onClick = {this.handleClick}>7</button>
+                <button name="8" onClick = {this.handleClick}>8</button>
+                <button name ="9" onClick = {this.handleClick}>9</button>
+                <button name ="+" onClick = {this.handleClick}>+</button>
+                <button name ="-" onClick = {this.handleClick}>-</button>
+                <button name ="/" onClick = {this.handleClick}> &divide;</button>
+                <button name ="*" onClick = {this.handleClick}>*</button>
+                <button name = "=" onClick = {this.handleClick}>=</button>
+                <button name = "clear" onClick = {this.handleClick}>"Ac"</button>
             </div>
         )
     }
